e04267a01aac6b5697786004a6670c4aec66182d
Jeff Yurkiw <jyurkiw@yahoo.com>
2012-08-12 21:32:00 -0700

    Added a combat log GUIStyle component to the combat runner to turn on wordwrap without borking the rest of the UI. Added the combat log component. Some sanity checking/smoke testing performed. It doesn't explode when you shove text into it. Left in some text code. Will remove later when there's content to push into the combat log.

--- Assets/Scripts/Combat/GUI/CombatRunner.cs
+++ Assets/Scripts/Combat/GUI/CombatRunner.cs
@@ -4,7 +4,8 @@ using System.Collections.Generic;
 [AddComponentMenu("PRPG/Combat/Combat Runner")]
 public class CombatRunner : MonoBehaviour {
 	public bool combatGUIActive = false;	///< Is the combat GUI active? Setting true displays the GUI.
-	public GUIStyle combatGUIStyle;			///< Current GUI style for the combat GUI
+	public GUIStyle combatGUIStyle;			///< Current GUI style for the combat GUI.
+	public GUIStyle combatLogGUIStyle;		///< Current GUI style for the combat log GUI.
 	
 	private CharacterManager playerParty;
 	private CharacterManager enemyParty;
@@ -12,6 +13,7 @@ public class CombatRunner : MonoBehaviour {
 	//GUI objects
 	private FrameList playerDisplay;
 	private Frame enemyList;
+	private FixedLineTextArea combatLog;
 	
 	/**
 	 * Initialize the combat runner for use.
@@ -50,6 +52,26 @@ public class CombatRunner : MonoBehaviour {
 		foreach(ClassedCombatActor actor in enemyParty.partyCharacters) {
 			enemyList.AddFrameItem(actor.Name);
 		}
+		
+		//init the enemy list so we can size the combat log
+		enemyList.Init();
+		
+		//initialize the combat log (10 px margin between enemy list and combat log)
+		//combat log is 12 lines high
+		float combatLogMargin = 10.0f;
+		float combatLogLines = 12.0f;
+		Rect combatLogDimensions = new Rect(0, 0, 0, 0);
+		combatLogDimensions.x = enemyList.Boundary.x + enemyList.Boundary.width + combatLogMargin;
+		combatLogDimensions.y = enemyList.Boundary.y;
+		combatLogDimensions.width = Screen.width - combatLogDimensions.x;
+		combatLog = new FixedLineTextArea(combatLogGUIStyle, combatLogDimensions);
+		combatLog.DisplayLines = 12;
+		
+		//TEST CODE
+		foreach(ClassedCombatActor actor in enemyParty.partyCharacters) {
+			combatLog.AddLine("A wild " + actor.Name + " appears!");
+		}
+		combatLog.AddLine("FIGHT!");
 	}
 	
 	public void temp() {
@@ -65,6 +87,7 @@ public class CombatRunner : MonoBehaviour {
 		if (combatGUIActive) {
 			playerDisplay.Draw();
 			enemyList.Draw();
+			combatLog.Draw();
 		}
 	}
 	
